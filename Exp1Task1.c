0344952c7cd603f1af0f0573e85605e2c7c61ebf
JeevanPoojary03 <jeevanpoojary@eng.rizvi.edu.in>
2024-10-23 20:57:51 +0530

    Add files via upload

---
+++ Exp1Task1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("Full Name\t:Jeevan Poojary\n");
+    printf("Age\t\t:18\n");
+    printf("Class\t\t:AIDS\n");
+    printf("Division\t:A\n");
+    printf("UIN\t\t:241A028\n");
+    return 0;
+}
