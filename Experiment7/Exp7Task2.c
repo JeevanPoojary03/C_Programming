753a0a6ecc684366a3ee4906055f4ec50f96ee16
JeevanPoojary03 <jeevanpoojary@eng.rizvi.edu.in>
2024-11-17 19:29:51 +0530

    Add files via upload

--- Experiment7/Exp7Task2.c
+++ Experiment7/Exp7Task2.c
@@ -1,46 +1,45 @@
-/*Aim:WAP to calculate sum of two matrix.
-
-Name: Jeevan Poojary
-UIN: 241A028
-Class: AI & DS
-Roll no: 24
-*/
 #include<stdio.h>
-
 int main(){
-    int arr1[50][50], brr1[50][50], crr1[50][50], i, j, n;
-
-    printf("\n\nAddition of two Matrices:\n");
-    printf("------------------------\n");
-    printf("Input the size of the square matrix: ");
-    scanf("%d", &n);
-
-    printf("Input elements in the first matrix:\n");
-    for (i=0; i<n; i++){
-        for (j=0; j<n; j++){
-            printf("element-[%d],[%d]:",i,j);
-            scanf("%d",&arr1[i][j]);
-        }
-    }
-    printf("Input elements in the second matrix:\n");
-    for (i=0; i<n; i++){
-        for (j=0; j<n; j++){
-            printf("element-[%d],[%d]:",i,j);
-            scanf("%d",&brr1[i][j]);
-        }
-    }
-
-    for (i=0; i<n; i++)
-        for (j=0;j<n; j++)
-            crr1[i][j] = arr1[i][j] + brr1[i][j];
-
-    printf("\nThe Addition of two matrix is:\n");
-    for (i=0; i<n; i++){
-        printf("\n");
-        for (j=0; j<n; j++){
-            printf("%d\t",crr1[i][j]);
-        }
-
-    return 0;
-    }
+int arr1[50][50], brr1[50][50], crr1[50][50], i, j, n;
+printf("\n\nAddition of two Matrices:\n");
+printf("------------------------\n");
+printf("Input the size of the square matrix: ");
+scanf("%d", &n);
+printf("Input elements in the first matrix:\n");
+for (i=0; i<n; i++){
+for (j=0; j<n; j++){
+printf("element-[%d],[%d]:",i,j);
+scanf("%d",&arr1[i][j]);
+}
+}
+printf("Input elements in the second matrix:\n");
+for (i=0; i<n; i++){
+for (j=0; j<n; j++){
+printf("element-[%d],[%d]:",i,j);
+scanf("%d",&brr1[i][j]);
+}
+}
+printf("\nThe First matrix is :\n");
+for (i = 0; i < n; i++) {
+printf("\n");
+for (j = 0; j < n; j++)
+printf("%d\t", arr1[i][j]);
+}
+printf("\nThe Second matrix is :\n");
+for (i = 0; i < n; i++) {
+printf("\n");
+for (j = 0; j < n; j++)
+printf("%d\t", brr1[i][j]);
+}
+for (i = 0; i < n; i++)
+for (j = 0; j < n; j++)
+crr1[i][j] = arr1[i][j] + brr1[i][j];
+printf("\nThe Addition of two matrix is : \n");
+for (i = 0; i < n; i++) {
+printf("\n");
+for (j = 0; j < n; j++)
+printf("%d\t", crr1[i][j]);
+}
+printf("\n\n");
+return 0;
 }
