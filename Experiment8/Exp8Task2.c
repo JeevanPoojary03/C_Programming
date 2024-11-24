363cee0f37034a6fdb831cbb56c842795e1da8d1
JeevanPoojary03 <jeevanpoojary@eng.rizvi.edu.in>
2024-11-24 15:57:20 +0530

    Add files via upload

---
+++ Experiment8/Exp8Task2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+char str[100];
+int i;
+printf("\t\t***Palindrome Finder***\n\n");
+printf("Enter Your String:");
+gets(str);
+for(i=0; i<strlen(str)-1; i++){
+if(str[i] !=str[strlen(str)-1-i]){
+printf("The String is NOT a Palindrome");
+return 0;
+}
+}
+printf("The String is a Palindrome");
+return 0;
+}
