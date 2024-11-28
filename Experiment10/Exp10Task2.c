772007eaeb80e610e1589eee168dc8fcb7ff30de
JeevanPoojary03 <jeevanpoojary@eng.rizvi.edu.in>
2024-11-28 20:46:52 +0530

    Add files via upload

---
+++ Experiment10/Exp10Task2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+int n;
+printf("\t\t** Array Reverser ** \n\n");
+// Input the size of the array
+printf("Enter the size of the array: ");
+scanf("%d", &n);
+int arr[n];
+int *ptr = arr; // Pointer to the array
+// Input the elements of the array
+printf("Enter the elements of the array:\n");
+for (int i = 0; i < n; i++) {
+printf("%d :", i);
+scanf("%d", ptr + i); // Input directly into array elements using pointer
+}
+// Print the elements in reverse order using pointer
+printf("\nArray elements in reverse order:\n");
+for (int i = n - 1; i >= 0; i--) {
+printf("%d ", *(ptr + i)); // Access elements using pointer
+}
+printf("\n");
+return 0;
+}
